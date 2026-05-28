d28b5701bae01b94dd6585b9c3429adbc1233197
BERENGEZ <BERENGEZ>
2026-05-28 17:21:54 +0200

    Integrated Javascript fetch counter into frontend asset

---
+++ lambda_function.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+
+#Initialize the DynamoDB client
+dynamodb = boto3.resource('dynamodb', region_name='eu-north-1') 
+table = dynamodb.Table('cloud-resume-counter') # Change to your region
+
+def lambda_handler(event, context):
+    try:
+        # Increment the visitor counter in DynamoDB
+        response = table.update_item(
+            Key={'id': 'visitors'},
+            UpdateExpression='SET version_count = if_not_exists(version_count, :start) + :inc',
+            ExpressionAttributeValues={
+                ':inc': 1,
+                ':start': 0
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        #Extract the updated count number
+        views = int(response['Attributes']['version_count'])
+        
+        #Return  a success response with CORS headers allowed
+        return{
+            'statusCode': 200,
+            'headers': {
+                'Access_Control_Allow_Origin': '*',
+                'Access_Control_Allow_Headers': 'Content-Type',
+                'Access_Control_Allow_Methods': 'GET, OPTIONS'
+            },
+            'body':json.dumps({'count': views})
+            }
+        
+    except Exception as e:
+        print(f"Error updating the counter: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': 'Internal Server Error'})
+        }
+               
+ #The Logic in this code is to wake up, read the counter from DynamoDB add +1 and save it back to DynamoDB. Then return the new count number as a response.
+ 
--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# 1. Tell Terraform we are connecting to AWS
+# 1. Tell Terraform to connect to AWS
 terraform {
   required_providers {
     aws = {
@@ -37,4 +37,120 @@ resource "aws_s3_bucket_public_access_block" "resume_bucket_block" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+#SEVERLESS BACKEND (AWS LAMBDA)
+
+#Automatically zip python code to be uploaded to AWS
+data "archive_file" "lambda_zip" {
+  type    = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+# Create an IAM Execution Role so Lambda has permission to run
+resource "aws_iam_role" "lambda_role" {
+  name = "cloud-resume-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+# Attach a secure security policy giving your Lambda role access to read/write DynamoDB
+resource "aws_iam_policy" "lambda_dynamodb_policy" {
+  name        = "cloud-resume-lambda-dynamodb-policy"
+  description = "Allows Lambda to interact with the DynamoDB counter table"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "dynamodb:UpdateItem",
+          "dynamodb:GetItem"
+        ]
+        Resource = aws_dynamodb_table.counter_table.arn
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
+
+# Define the live AWS Lambda Function resource
+resource "aws_lambda_function" "counter_lambda" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "cloud-resume-counter-function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+# PUBLIC API ENTRANCE (API GATEWAY)
+
+# Create an HTTP API Gateway mapping entry point
+resource "aws_apigatewayv2_api" "lambda_api" {
+  name          = "cloud-resume-api"
+  protocol_type = "HTTP"
+  
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET", "OPTIONS"]
+    allow_headers = ["content-type"]
+  }
+}
+
+# Connect the API Gateway route directly to our Lambda backend function
+resource "aws_apigatewayv2_integration" "api_lambda_integration" {
+  api_id           = aws_apigatewayv2_api.lambda_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.counter_lambda.arn
+}
+
+resource "aws_apigatewayv2_route" "api_route" {
+  api_id    = aws_apigatewayv2_api.lambda_api.id
+  route_key = "GET /counter"
+  target    = "integrations/${aws_apigatewayv2_integration.api_lambda_integration.id}"
+}
+
+# Create a live environment deployment stage for our API
+resource "aws_apigatewayv2_stage" "api_stage" {
+  api_id      = aws_apigatewayv2_api.lambda_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# Give permission to API Gateway to trigger your Lambda function
+resource "aws_lambda_permission" "api_gw_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.counter_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
+}
+
+# Output your live public API endpoint string straight to your terminal screen
+output "api_endpoint" {
+  value       = "${aws_apigatewayv2_api.lambda_api.api_endpoint}/counter"
+  description = "The public API gateway endpoint URL to fetch your live visitor count"
 }
